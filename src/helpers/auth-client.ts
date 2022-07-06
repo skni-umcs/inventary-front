f8401ee7f3669f53a35f135b955bf6963c3b85b8
stanislaw.czajka <stanislaw.czajka@skni.umcs.pl>
2022-07-06 13:47:40 +0200

    Added a feature which checks if current stored JWT is still valid

--- src/helpers/auth-client.ts
+++ src/helpers/auth-client.ts
@@ -1,3 +1,22 @@
+interface jwt {
+    exp: number,
+    fresh?: boolean,
+    iat?: number,
+    jti?: string,
+    nbf?: number,
+    sub?: string,
+    type?: string
+}
+
+const parseToken = (token: string): jwt => {
+    try {
+        return JSON.parse(atob(token.split('.')[1]))
+    } catch(e) {
+        console.error('Couldn\'t parse JWT token. Try signing out and in again!');
+        return {exp: 0}
+    }
+}
+
 export default {
     getJwt() {
         return localStorage.getItem('jwt');
@@ -8,4 +27,16 @@ export default {
     clearJwt() {
         localStorage.removeItem('jwt');
     },
+    checkValid() {
+        const token = localStorage.getItem('jwt');
+        if(token) {
+            const parsedToken = parseToken(token);
+            if(parsedToken.exp < Date.now() / 1000) {
+                this.clearJwt();
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
